c3e4f046d39b3d64f7de917395956455341dd487
Vladimir Radionovsky <vova@MacBook-Pro-Vladimir.local>
2020-02-11 23:14:36 +0300

    got mail to work

--- dist/phpmailer/mail.php
+++ dist/phpmailer/mail.php
@@ -6,48 +6,53 @@ $response = array(
     'message' => 'Form submission failed, please try again.' 
 ); 
 
+
 function smtpmailer($to, $from, $from_name, $subject, $body)
     {
-        if(isset($_POST['name']) || isset($_POST['email']) || isset($_POST['subject']) || isset($_POST['msg'])){ 
-        $mail = new PHPMailer();
-        $mail->IsSMTP();
-        $mail->SMTPAuth = true; 
- 
-        $mail->SMTPSecure = 'ssl'; 
-        $mail->Host = 'dayosh.ru';
-        $mail->Port = 465;  
-        $mail->Username = 'send@dayosh.ru';
-        $mail->Password = 'wydcaw-mivqyn-mukTi0';   
-   
-   //   $path = 'reseller.pdf';
-   //   $mail->AddAttachment($path);
-   
-        $mail->IsHTML(true);
-        $mail->From="mail@dayosh.ru";
-        $mail->FromName=$from_name;
-        $mail->Sender=$from;
-        $mail->AddReplyTo($from, $from_name);
-        $mail->Subject = "{$_POST['subject']}";
-        $mail->Body = "{$_POST['name']} {$_POST['msg']}";
-        $mail->AltBody = "{$_POST['msg']}";
-        $mail->AddAddress($to);
-        if(!$mail->Send())
-        {
-            $response['message'] = 'Form data submitted successfully!'; 
+        // if(isset($_POST['name']) || isset($_POST['email']) || isset($_POST['subject']) || isset($_POST['msg'])){ 
+            $mail = new PHPMailer();
+            $mail->IsSMTP();
+            $mail->SMTPAuth = true; 
+    
+            $mail->SMTPSecure = 'ssl'; 
+            $mail->Host = 'dayosh.ru';
+            $mail->Port = 465;  
+            $mail->Username = 'send@dayosh.ru';
+            $mail->Password = 'wydcaw-mivqyn-mukTi0';   
+    
+    //   $path = 'reseller.pdf';
+    //   $mail->AddAttachment($path);
+    
+            $mail->IsHTML(true);
+            $mail->From="mail@dayosh.ru";
+            $mail->FromName=$from_name;
+            $mail->Sender=$from;
+            $mail->AddReplyTo($from, $from_name);
+            $mail->Subject = "{$_POST['subject']}";
+            $mail->Body = "{$_POST['name']} {$_POST['msg']}";
+            $mail->AltBody = "{$_POST['msg']}";
+            $mail->AddAddress($to);
+            if(!$mail->Send())
+            {
+                global $response;
+                $response = array( 
+                    'status' => 0, 
+                    'message' => 'Form submission failed, please try again.' 
+                ); 
+            }
+            else 
+            {
+                global $response;
+                $response = array( 
+                    'status' => 1, 
+                    'message' => 'Form data submitted successfully!' 
+                ); 
+                            
+            }
+
         }
-        else 
-        {
-            $response['message'] = 'Form data submitted successfully!'; 
-        }
-    }
-    }
-    // if(isset($_POST['name']) || isset($_POST['email']) || isset($_POST['subject']) || isset($_POST['msg'])){ 
-    //     $error ="111";
-    //         return $error; 
-    // } else {
-    //      $error ="222";
-    //         return $error; 
     // }
+    
     
     $to   = 'alkelk@mail.ru';
     // $from = 'send@dayosh.ru';
@@ -58,10 +63,8 @@ function smtpmailer($to, $from, $from_name, $subject, $body)
     // $name = $_POST['name'];
     // $subj = $_POST['subject'];
     // $msg = $_POST['msg'];
-
     
+    smtpmailer($to,$from, $name ,$subj, $msg);
     echo json_encode($response);
-    $error=smtpmailer($to,$from, $name ,$subj, $msg);
-
-    echo $myJSON;   
+  
 ?>
