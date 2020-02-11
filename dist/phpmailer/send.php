ecb26171f8539d5c493d5c886426c3dc0a6e6f3f
Vladimir Radionovsky <vova@MacBook-Pro-Vladimir.local>
2020-02-11 16:44:48 +0300

    added php mail

---
+++ dist/phpmailer/send.php
@@ -0,0 +1,54 @@
+<?php
+require "PHPMailer/PHPMailerAutoload.php";
+
+function smtpmailer($to, $from, $from_name, $subject, $body)
+    {
+        $mail = new PHPMailer();
+        $mail->IsSMTP();
+        $mail->SMTPAuth = true; 
+ 
+        $mail->SMTPSecure = 'ssl'; 
+        $mail->Host = 'mail.dayosh.ru';
+        $mail->Port = 465;  
+        $mail->Username = 'send@dayosh.ru';
+        $mail->Password = 'wydcaw-mivqyn-mukTi0';   
+   
+   //   $path = 'reseller.pdf';
+   //   $mail->AddAttachment($path);
+   
+        $mail->IsHTML(true);
+        $mail->From="send@dayosh.ru";
+        $mail->FromName=$from_name;
+        $mail->Sender=$from;
+        $mail->AddReplyTo($from, $from_name);
+        $mail->Subject = $subject;
+        $mail->Body = $body;
+        $mail->AddAddress($to);
+        if(!$mail->Send())
+        {
+            $error ="Please try Later, Error Occured while Processing...";
+            return $error; 
+        }
+        else 
+        {
+            $error = "Thanks You !! Your email is sent.";  
+            return $error;
+        }
+    }
+    
+    $to   = 'alkelk@mail.ru';
+    $from = 'send@dayosh.ru';
+    $name = 'tets';
+    $subj = 'PHPMailer 5.2 testing from DomainRacer';
+    $msg = 'This is mail about testing mailing using PHP.';
+    
+    $error=smtpmailer($to,$from, $name ,$subj, $msg);
+    
+?>
+
+<html>
+    <body style="background: black;">
+        <center><h2 style="padding-top:70px;color: white;"><?php echo $error; ?></h2></center>
+    </body>
+    
+</html>
