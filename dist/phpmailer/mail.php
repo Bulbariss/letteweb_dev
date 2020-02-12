b4705fccb49d9eb1cd0b45058f2c2f55eb4cf571
Vladimir Radionovsky <vova@MacBook-Pro-Vladimir.local>
2020-02-12 12:39:07 +0300

    got rid of jquery

--- dist/phpmailer/mail.php
+++ dist/phpmailer/mail.php
@@ -6,52 +6,43 @@ $response = array(
     'message' => 'Form submission failed, please try again.' 
 ); 
 
+$mail = new PHPMailer();
+$mail->IsSMTP();
+$mail->SMTPAuth = true; 
 
-function smtpmailer($to, $from, $from_name, $subject, $body)
-    {
-        // if(isset($_POST['name']) || isset($_POST['email']) || isset($_POST['subject']) || isset($_POST['msg'])){ 
-            $mail = new PHPMailer();
-            $mail->IsSMTP();
-            $mail->SMTPAuth = true; 
-    
-            $mail->SMTPSecure = 'ssl'; 
-            $mail->Host = 'dayosh.ru';
-            $mail->Port = 465;  
-            $mail->Username = 'send@dayosh.ru';
-            $mail->Password = 'wydcaw-mivqyn-mukTi0';   
-    
-    //   $path = 'reseller.pdf';
-    //   $mail->AddAttachment($path);
-    
-            $mail->IsHTML(true);
-            $mail->From="mail@dayosh.ru";
-            $mail->FromName=$from_name;
-            $mail->Sender=$from;
-            $mail->AddReplyTo($from, $from_name);
-            $mail->Subject = "{$_POST['subject']}";
-            $mail->Body = "{$_POST['name']} {$_POST['msg']}";
-            $mail->AltBody = "{$_POST['msg']}";
-            $mail->AddAddress($to);
-            if(!$mail->Send())
-            {
-                global $response;
-                $response = array( 
-                    'status' => 0, 
-                    'message' => 'Form submission failed, please try again.' 
-                ); 
-            }
-            else 
-            {
-                global $response;
-                $response = array( 
-                    'status' => 1, 
-                    'message' => 'Form data submitted successfully!' 
-                ); 
-                            
-            }
+$mail->SMTPSecure = 'ssl'; 
+$mail->Host = 'dayosh.ru';
+$mail->Port = 465;  
+$mail->Username = 'send@dayosh.ru';
+$mail->Password = 'wydcaw-mivqyn-mukTi0';   
 
-        }
-    // }
+$mail->IsHTML(true);
+$mail->From="mail@dayosh.ru";
+$mail->FromName="mail@dayosh.ru";
+$mail->Sender="mail@dayosh.ru";
+$mail->AddReplyTo("mail@dayosh.ru", "mail@dayosh.ru");
+$mail->Subject = "{$_POST['subject']}";
+$mail->Body = "{$_POST['name']} {$_POST['msg']}";
+$mail->AltBody = "{$_POST['msg']}";
+$mail->AddAddress('alkelk@mail.ru');
+
+if(!$mail->Send())
+{
+    global $response;
+    $response = array( 
+        'status' => 0, 
+        'message' => 'Form submission failed, please try again.' 
+    ); 
+}
+else 
+{
+    global $response;
+    $response = array( 
+        'status' => 1, 
+        'message' => 'Form data submitted successfully!' 
+    ); 
+                
+}
     
     
     $to   = 'alkelk@mail.ru';
@@ -64,7 +55,6 @@ function smtpmailer($to, $from, $from_name, $subject, $body)
     // $subj = $_POST['subject'];
     // $msg = $_POST['msg'];
     
-    smtpmailer($to,$from, $name ,$subj, $msg);
     echo json_encode($response);
   
 ?>
